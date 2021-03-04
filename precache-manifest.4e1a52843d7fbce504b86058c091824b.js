f9b218bf3350491119227a9ee593b9bdbdb4754f
DPK <dougkvamme@gmail.com>
2021-03-03 19:04:37 -0600

    Updates

---
+++ precache-manifest.4e1a52843d7fbce504b86058c091824b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6361ec988877489361ffec3741d175da",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "095986ea334b43c830b1",
+    "url": "/Portfolio/static/css/2.e25d4c8b.chunk.css"
+  },
+  {
+    "revision": "b5bd3e16b8a482c225a1",
+    "url": "/Portfolio/static/css/main.2f190406.chunk.css"
+  },
+  {
+    "revision": "095986ea334b43c830b1",
+    "url": "/Portfolio/static/js/2.25149220.chunk.js"
+  },
+  {
+    "revision": "b5bd3e16b8a482c225a1",
+    "url": "/Portfolio/static/js/main.58c79226.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
